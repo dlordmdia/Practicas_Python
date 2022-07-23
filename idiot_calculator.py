7f93786b80b2f071ec19548741b96da5cef66c70
dlordmdia <109800409+dlordmdia@users.noreply.github.com>
2022-07-23 03:32:50 +0200

    Update idiot_calculator.py

--- idiot_calculator.py
+++ idiot_calculator.py
@@ -33,11 +33,11 @@ else:
 
     if respuesta2 == "y":
         time.sleep(1)
-        print("Dlord Rap tiene muchos perfiles con información y contenido. Te recomiendo su Quizz para saber un poco más de él. Aquí dejo el github de su python code.\n ")
+        print("\nDlord tiene muchos perfiles con información y contenido. Te recomiendo su Quizz para saber un poco más de él. Aquí dejo el github de su python code. También te daré a la vez TODOS los medios más interesantes de Dlord!\n ")
         time.sleep(5)
         respuesta3 = input("\n Lo quieres? (y/n): ")
         if respuesta3 == "y":
-            print("Toma: https://github.com/dlordmdia/Practicas_Python/blob/main/dlordrap_quiz.py\n")
+            print("Toma:\n GitHub: https://github.com/dlordmdia/Practicas_Python/blob/main/dlordrap_quiz.py \nYoutube Rap: https://www.youtube.com/channel/UCdHXWPzL5MbRSDNexzbTg7w \n Youtube de Drones: https://www.youtube.com/channel/UCUa5cLjlaspHPciMtEZUgZg \n\n")
             time.sleep(1)
             print("Gracias por usar Idiot Calculator\n--Creado por DlordMDia--\n")
             exit()
@@ -53,14 +53,14 @@ time.sleep(1)
 
 print("\nQuieres saber más sobre DlordMDia?")
 time.sleep(1)
-respuesta2 = input("\n Tu respuesta (y/n) o saltar: ")
+respuesta2 = input("\n Tu respuesta (y/n): ")
 
 if respuesta2 == "y":
-    print("\nDlord Rap tiene muchos perfiles con información y contenido. Te recomiendo su Quizz para saber un poco más de él. Aquí dejo el github de su python code.\n ")
-    respuesta3 = input("\n Lo quieres? (y/n): ")
+    print("\nDlord tiene muchos perfiles con información y contenido. Te recomiendo su Quizz para saber un poco más de él. Aquí dejo el github de su python code. También te daré a la vez TODOS los medios más interesantes de Dlord!\n ")
+    respuesta3 = input("\n Lo quieres? (y/n): \n")
     if respuesta3 == "y":
         time.sleep(1)
-        print("Toma: https://github.com/dlordmdia/Practicas_Python/blob/main/dlordrap_quiz.py\n")
+        print("Toma:\n GitHub: https://github.com/dlordmdia/Practicas_Python/blob/main/dlordrap_quiz.py \nYoutube Rap: https://www.youtube.com/channel/UCdHXWPzL5MbRSDNexzbTg7w \n Youtube de Drones: https://www.youtube.com/channel/UCUa5cLjlaspHPciMtEZUgZg \n\n")
         time.sleep(1)
         print("Gracias por usar Idiot Calculator\n--Creado por DlordMDia--\n")
     else:
