5abe4cac9cdc9f58d93d84b12378e8bc04c7b5ce
dlordmdia <shnuffi2.02@gmail.com>
2022-07-25 16:36:15 +0200

    Reotques

--- lista-compras.py
+++ lista-compras.py
@@ -10,7 +10,9 @@ precio_total = 0.0
 
 print("\nQue quieres hacer?\n")
 time.sleep(2)
-print("\na) Añadir cosas\nb) Ver lista\nc) ¡¡Cerrar lista de compras!!\nd) Abrir Calculadora\ne) Ver precio Total")
+menu1 = "\na) Añadir cosas\nb) Ver lista\nc) Ver código mientras tu compra\nd) Añadir Precios\ne) Ver precio Total\nf) Eliminar Objeto de la lista\ng) Eliminar Precio\n\nPresiona /ENTER\ para salir de la lista de compras"
+
+print(menu1)
 
 while True:
     inicio = input("- ")
@@ -23,7 +25,7 @@ while True:
                 print("Saliendo...")
                 anadiendo = False
                 os.system('cls' if os.name=='nt' else 'clear')
-                print("\na) Añadir cosas\nb) Ver lista\nc) ¡¡Cerrar lista de compras!!\nd) Abrir Calculadora\ne) Ver precio Total")
+                print(menu1)
 
             else:
                 lista.append(objeto)
@@ -36,14 +38,16 @@ while True:
         saleave = input("\n\n-x to Leave:\n- ")
         if saleave == "x":
             os.system('cls' if os.name=='nt' else 'clear')
-            print("\na) Añadir cosas\nb) Ver lista\nc) ¡¡Cerrar lista de compras!!\nd) Abrir Calculadora\ne) Ver precio Total")
-    
+            print(menu1)
+
     elif inicio == "c":
         os.system('cls' if os.name=='nt' else 'clear')
-        print("Saliendo...")
+        print("Redirigiendote a GitHub...")
         time.sleep(1)
-        print("\n\nGracias por usar Lista de Compras de @dlordmdia\n")
-        exit()
+        webbrowser.open("https://github.com/dlordmdia")
+        time.sleep(3)
+        os.system('cls' if os.name=='nt' else 'clear')
+        print(menu1)
 
     elif inicio == "d":
         os.system('cls' if os.name=='nt' else 'clear')
@@ -54,7 +58,7 @@ while True:
                 print("Saliendo...")
                 calculando = False
                 os.system('cls' if os.name=='nt' else 'clear')
-                print("\na) Añadir cosas\nb) Ver lista\nc) ¡¡Cerrar lista de compras!!\nd) Abrir Calculadora\ne) Ver precio Total")
+                print(menu1)
 
             else:
                 precio_total += float(precio)
@@ -64,4 +68,39 @@ while True:
         xleave = input(f"Tu precio total és: {precio_total}€\n\n -x to Leave:\n- ")
         if xleave == "x":
             os.system('cls' if os.name=='nt' else 'clear')
-            print("\na) Añadir cosas\nb) Ver lista\nc) Cerrar lista de compras!!\nd) Abrir Calculadora\ne) Ver precio Total")
+            print(menu1)
+
+    elif inicio == "f":
+        anadiendo = True
+        os.system('cls' if os.name=='nt' else 'clear')
+        print("\n-x to Leave | Que Objeto quieres eliminar de la lista?")
+        while anadiendo:
+            print("Tienes: ")
+            for item in lista:
+                print(f"- {item}")
+
+            item_to_remove = input("\nEliminas: ")
+            if item_to_remove == "x":
+                os.system('cls' if os.name=='nt' else 'clear')
+                print(menu1)
+                anadiendo = False
+            
+            else:
+                if item_to_remove in lista:
+                    for item in lista:
+                        if item == item_to_remove:
+                            lista.pop(lista.index(item_to_remove))
+                    print("Eliminado!!\n")
+                
+                else:
+                    print("Este objeto no está en la lista\n")
+
+    else:
+        print("\nGracias por usar la lista de compras de dlordmdia!\n\n")
+        time.sleep(3)
+        print("Redirigiendote a GitHub (@dlordmdia)...")
+        time.sleep(1)
+        webbrowser.open("https://github.com/dlordmdia")
+        time.sleep(2)
+        print("Abierto correctamente!\n")
+        break
