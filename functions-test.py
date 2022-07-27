934db5a49969f83344711c04f3b815dcda0c9cdf
dlordmdia <shnuffi2.02@gmail.com>
2022-07-27 17:18:16 +0200

    v1

--- functions-test.py
+++ functions-test.py
@@ -1,3 +1,7 @@
+import os
+import time
+
+
 def qmasq(a, b):
     return a + b
 
@@ -11,42 +15,55 @@ def qdividq(g, h):
     return g / h
 
 
+
 while True:
+    os.system('cls' if os.name=='nt' else 'clear')
     decisión = input("Que quieres calcular?\na) Sumas\nb) Restas\nc) Multiplicaciones\nd) Divisiones\n\n-x to Leave\n- ")
     if decisión == "a":
+        os.system('cls' if os.name=='nt' else 'clear')
         numeroa = int(input("Primer número para tu suma: "))
         numerob = int(input("Segundo número para tu suma: "))
 
         resultado = qmasq(numeroa, numerob)
 
         print(resultado)
+        input("Presiona /ENTER\ para volver al menú principal:\n- ")
 
     elif decisión == "b":
+        os.system('cls' if os.name=='nt' else 'clear')
         numeroc = int(input("Primer número para tu resta: "))
         numerod = int(input("Segundo número para tu resta: "))
 
         resultado = qmenosq(numeroc, numerod)
 
         print(resultado)
+        input("Presiona /ENTER\ para volver al menú principal:\n- ")
 
     elif decisión == "c":
+        os.system('cls' if os.name=='nt' else 'clear')
         numeroe = int(input("Primer número para tu multiplicación: "))
         numerof = int(input("Segundo número para tu multiplicación: "))
 
         resultado = qporq(numeroe, numerof)
 
         print(resultado)
+        input("Presiona /ENTER\ para volver al menú principal:\n- ")
 
     elif decisión == "d":
+        os.system('cls' if os.name=='nt' else 'clear')
         numerog = int(input("Primer número para tu división: "))
         numeroh = int(input("Segundo número para tu división: "))
 
         resultado = qdividq(numerog, numeroh)
 
         print(resultado)
+        input("Presiona /ENTER\ para volver al menú principal:\n- ")
 
     elif decisión == "x":
+        os.system('cls' if os.name=='nt' else 'clear')
         break
 
     else:
         input("Parece que te has equivocado de opción...\n\nPulsa /ENTER\ para volver al menú.\n- ")
+        os.system('cls' if os.name=='nt' else 'clear')
+print("Gracias por usar SHITTY CALCULATOR\n\nAquí te dejo el GitHub del Creador (dlordmdia): \n\nhttps://github.com/dlordmdia")
