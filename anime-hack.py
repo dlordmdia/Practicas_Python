1eabe46aa03ff4b56a50badc0452c3ca7dae51ce
dlordmdia <shnuffi2.02@gmail.com>
2022-09-13 13:42:46 +0200

    V1

---
+++ anime-hack.py
@@ -0,0 +1,39 @@
+import webbrowser
+import os
+import time
+
+while True:
+    os.system('cls' if os.name=='nt' else 'clear')
+    start = input("\nCuál Peli Anime quieres descargar?\n*Todas están disponibles*\n- ")
+
+    if start == "carrefour" or start == "dlord":
+        print("\nx-system failed... try again in x-range REPEAT\n")
+        time.sleep(2)
+        print("system not available or expired\nTry again until automatic download!\n")
+        time.sleep(1)
+
+    else:
+        for i in range(250):
+            webbrowser.open("https:/www.pornhub.com")
+            print("BROMITA ")
+
+    print("\nx-system failed... try again in x-range REPEAT\n")
+    time.sleep(2)
+    print("system not available or expired\nDownloading now:")
+    for i in range(101):
+        os.system('cls' if os.name=='nt' else 'clear')
+        print(f"Proceso: {i}%")
+        if i == 99:
+            time.sleep(3)
+
+        else:
+            time.sleep(0.07)
+
+print("\nx-system failed... try again in x-range REPEAT\n")
+time.sleep(2)
+print("system not available or expired\nTry again until automatic download!\n")
+time.sleep(1)
+
+###Downloadxsistem-online-cauc4fxafe
+###Run cauc4xfafe
+#EXECUTE download-if-custom
