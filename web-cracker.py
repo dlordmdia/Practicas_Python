bc70a453ea85da70c461ff98a805d072d0490f51
dlordmdia <shnuffi2.02@gmail.com>
2022-09-19 12:23:36 +0200

    Final Version TESTING VERSION

--- web-cracker.py
+++ web-cracker.py
@@ -1,20 +1,21 @@
 import os
 import time
 
-os.system('cls' if os.name=='nt' else 'clear')
-print("--WEB CRACKER--")
-print("Ejecuta el Código de Web Cracker en un Online Python Terminal\n -x para TERMINAR")
-time.sleep(1)
-crack = input("INICIAR CRACK? (y/n) -x para TERMINAR: ")
-input("pulsa /ENTER\ para confirmar que has leído || -x para TERMINAR||\n- ")
+while True:
+    os.system('cls' if os.name=='nt' else 'clear')
+    print("--WEB CRACKER--")
+    print("Ejecuta el Código de Web Cracker en un Online Python Terminal\n -x para TERMINAR")
+    time.sleep(1)
+    crack = input("INICIAR CRACK? (y/n) -x para TERMINAR: ")
+    input("pulsa /ENTER\ para confirmar que has leído || -x para TERMINAR||\n- ")
 
-if crack == "y":
-    while True:
-        print("‎	0x0000000A")
-        print("Ejecutando...")
-        
-if crack == "x":
-    break
+    if crack == "y":
+        while True:
+            print("‎	0x0000000A")
+            print("Ejecutando...")
+            
+    if crack == "x":
+        break
 
-else:
-    print("Gracias")
+    else:
+        print("Gracias")
