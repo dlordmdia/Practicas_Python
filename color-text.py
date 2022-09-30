1c3f77f9c564af7fc295b044d321c96f5b350d2c
dlordmdia <shnuffi2.02@gmail.com>
2022-09-30 09:51:13 +0200

    v1

---
+++ color-text.py
@@ -0,0 +1,26 @@
+import colorama
+from colorama import Fore
+import string
+
+print("----CUSTOM-TEXT----")
+while True:
+    color = input("Tu color:\n\na) rojo\nb) verde\nc) amarillo\nd) rosa\ne) azul\n\n-x to Exit\n-  ")
+    custom = input("Tu texto: ")
+
+    if color == "a":
+        print(Fore.RED + f'{custom}')
+
+    if color == "b":
+        print(Fore.GREEN + f'{custom}')
+
+    if color == "c":
+        print(Fore.YELLOW + f'{custom}')
+
+    if color == "d":
+        print(Fore.PINK + f'{custom}')
+
+    if color == "e":
+        print(Fore.BLUE + f'{custom}')
+    
+    if color == "x":
+        break
